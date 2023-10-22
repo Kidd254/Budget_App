64af7aa90e68042dbbbbb33f0e9045848d1bc837
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-22 18:26:45 +0300

    Add validations and associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :categories 
+  has_many :payments
+  validates :email, presence: true, uniqueness: true
+  validates :password, presence: true, length: { minimum: 8 }
 end
