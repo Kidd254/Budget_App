cc5a29b481d07299695b6240c6fd4f3fa83d9dec
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-22 18:47:26 +0300

    Fix offenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  has_many :categories 
+  has_many :categories
   has_many :payments
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true, length: { minimum: 8 }
