c7f4f450df41971c178629682145e22349ecdfaa
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-22 17:44:52 +0300

    Add a foreign key that references user in payments table and another foreign key that references categories in payments table

--- db/migrate/20231022143333_add_category_to_payments.rb
+++ db/migrate/20231022143333_add_category_to_payments.rb
@@ -1,5 +1,5 @@
 class AddCategoryToPayments < ActiveRecord::Migration[7.0]
   def change
-    add_reference :payments, :category, null: false, foreign_key: true
+    add_reference :payments, :category, null: false, foreign_key: { to_table: :categories}
   end
 end
