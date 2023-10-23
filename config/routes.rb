0ef1407a3ddada2da609d6a238352245c12806ad
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-23 20:06:42 +0300

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   devise_for :users
   resources :categories, only: [:index, :show]
   root "categories#index"
-  resources :payments, only: [:index]
+  resources :payments, only: [:index, :new, :create]
 end
