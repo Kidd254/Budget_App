f48654497e0878f29f2afb26291fec448d733be9
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-23 20:03:58 +0300

    Add methods for creating and saving transactions

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,22 @@
 class PaymentsController < ApplicationController
+  before_action :authenticate_user!
+  def new
+    @payment = Payment.new
+  end
+
+  def create
+    @payment = Payment.new(payment_params)
+    @payment.author_id = current_user.id
+    if @payment.save
+      redirect_to category_path(@payment.category)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def payment_params
+    params.require(:payment).permit(:name, :amount, :category_id)
+  end
 end
