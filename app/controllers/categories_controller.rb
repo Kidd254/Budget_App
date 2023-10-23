75c5e4dcc55e3177000dda95818ccf39ec43526b
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-23 20:03:12 +0300

    Add methods for dispaying transactions details

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,8 +1,14 @@
 class CategoriesController < ApplicationController
-    before_action :authenticate_user!
-    
-    def index
-        @categories = Category.all
-        @payments = Payment.all
-    end
+  before_action :authenticate_user!
+
+  def index
+    @categories = Category.all
+    @payments = Payment.all
+  end
+
+  def show
+    @category = Category.find(params[:id])
+    @payments = @category.payments.order(created_at: :desc)
+    @total_amount = @payments.sum(:amount)
+  end
 end
