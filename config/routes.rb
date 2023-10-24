25efad3188343b34f7b30a8dd496f67c38b64d14
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-24 10:57:47 +0300

    Modify the routes to reflect the methods in controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :categories, only: [:index, :show]
+  resources :categories, only: [:index, :show, :new, :create]
   root "categories#index"
   resources :payments, only: [:index, :new, :create]
 end
