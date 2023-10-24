9e02809ff17a2a2c8ed153ea0502068ec3575156
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-25 00:56:15 +0300

    Modify the routes to direct to  landing page upon render on localhost

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: 'sessions' # Remove 'users/' from here
+  }
+  
+  get 'landing', to: 'welcome#landing'
   resources :categories, only: [:index, :show, :new, :create]
-  root "categories#index"
   resources :payments, only: [:index, :new, :create]
+  root "welcome#landing"
 end
