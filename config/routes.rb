6a7126760ff5ad3d1a8612dfd71181b73f6019e9
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-25 23:10:49 +0300

    Modify the welcome page to match the provided wireframe

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'sessions',
-    registrations: 'users/sessions/registrations'
+    registrations: 'users/sessions/registrations',
+    confirmations: 'users/sessions/confirmations'
   }
   
-  get 'landing', to: 'welcome#landing'
-  resources :categories, only: [:index, :show, :new, :create]
-  resources :payments, only: [:index, :new, :create]
-  root "welcome#landing"
+  devise_scope :user do  # Wrap your routes in a devise_scope block
+    get 'landing', to: 'welcome#landing'
+    resources :categories, only: [:index, :show, :new, :create]
+    resources :payments, only: [:index, :new, :create]
+    root "welcome#landing"
+    get '/resend_confirmation', to: 'users/sessions/confirmations#resend', as: :resend_confirmation
+  end
 end
