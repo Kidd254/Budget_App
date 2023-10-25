7571329d6d80017932ba29acdc418834dae68f90
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-25 15:24:57 +0300

    Add routes to registration page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    sessions: 'sessions' # Remove 'users/' from here
+    sessions: 'sessions',
+    registrations: 'users/sessions/registrations'
   }
   
   get 'landing', to: 'welcome#landing'
