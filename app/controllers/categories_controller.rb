980e268313697339876828b349d1e74a42bbc66c
Lawrence Muema Kioko <lorenkioko@gmail.com>
2023-10-26 13:44:33 +0300

    Fix linter

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -28,58 +28,7 @@ class CategoriesController < ApplicationController
 
   private
 
-  class CategoriesController < ApplicationController
-    before_action :authenticate_user!, except: %i[index show]
-
-    def index
-      @categories = current_user.categories
-      @payments = Payment.all
-    end
-
-    def show
-      @category = Category.find(params[:id])
-      @payments = @category.payments.order(created_at: :desc)
-      @total_amount = @payments.sum(:amount)
-    end
-
-    def new
-      @category = Category.new
-    end
-
-    def create
-      @category = Category.new(category_params)
-      @category.author_id = current_user.id
-      if @category.save
-        redirect_to categories_path
-      else
-        render :new
-      end
-    end
-
-    private
-
-    def category_params
-      params.require(:category).permit(:name, :icon)
-    end
+  def category_params
+    params.require(:category).permit(:name, :icon, :description)
   end
-
-    def new
-      @category = Category.new
-    end
-
-    def create
-      @category = Category.new(category_params)
-      @category.author_id = current_user.id
-      if @category.save
-        redirect_to categories_path
-      else
-        render :new
-      end
-    end
-
-    private
-
-    def category_params
-      params.require(:category).permit(:name, :icon)
-    end
-  end
+end
